d5ae19a280372bbfd933c5cb98a93bb536ad5fb0
Eugene Wang <fengbaoxp@gmail.com>
2014-09-17 10:37:41 +0800

    添加git别名

--- git/config.sh
+++ git/config.sh
@@ -29,3 +29,9 @@ git config --global push.default upstream
 git config --global core.autocrlf input
 
 git config --global core.excludesfile ~/.gitignore_global
+
+# 别名配置
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
